613b324259a4aad1576ba31dc2be674bcf4509af
Daniil <dkorz.33@gmail.com>
2023-09-24 15:20:51 +0300

    all commit

---
+++ example.py
@@ -0,0 +1,29 @@
+class Car:
+    __color = ""
+    weight = ""
+
+    def set(self, color, weight):
+        self.__color = color
+        self.weight = weight
+
+
+class BMW(Car):
+    isM_model = False
+
+    def set(self, color, weight, isM_model):
+        self._Car__color = color
+        self.weight = weight
+        self.isM_model = isM_model
+
+
+class Mercedes(Car):
+    isMaybach = False
+
+
+x3 = BMW()
+x3.set("White", 2400, False)
+print(x3._Car__color)
+
+m3 = BMW()
+m3.set("Blue", 1400, True)
+print(m3.isM_model)
